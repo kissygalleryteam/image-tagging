03ec5d6449c0b85884f48c2424a5728bd2fbdd37
乔福 <qiaofu@taobao.com>
2013-05-13 21:37:35 +0800

    fisrt commit

---
+++ 1.0/meta/alias.js
@@ -0,0 +1,3 @@
+config({
+    'gallery/image-tagging/index': {alias: ['gallery/image-tagging/1.0/index']}
+});
---
+++ 1.0/meta/modules.js
@@ -0,0 +1,3 @@
+config({
+    'gallery/image-tagging/index': {requires: ['node','base']}
+});
