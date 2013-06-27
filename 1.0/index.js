c148af3c8a145e51710b34e1cd024ae3fd2b7a47
乔福 <qiaofu@taobao.com>
2013-06-27 11:16:35 +0800

    修改index.js

--- 1.0/index.js
+++ 1.0/index.js
@@ -1,29 +1,11 @@
 /**
  * @fileoverview 请修改组件描述
  * @author 乔福<qiaofu@taobao.com>
- * @module image-tagging
+ * @module ImageTagging
  **/
-KISSY.add(function (S, Node,Base) {
-    var EMPTY = '';
-    var $ = Node.all;
-    /**
-     * 请修改组件描述
-     * @class Image-tagging
-     * @constructor
-     * @extends Base
-     */
-    function Image-tagging(comConfig) {
-        var self = this;
-        //调用父类构造函数
-        Image-tagging.superclass.constructor.call(self, comConfig);
-    }
-    S.extend(Image-tagging, Base, /** @lends Image-tagging.prototype*/{
-
-    }, {ATTRS : /** @lends Image-tagging*/{
-
-    }});
-    return Image-tagging;
-}, {requires:['node', 'base']});
+KISSY.add(function (S, ImageTagging) {
+    return ImageTagging;
+}, {requires:['./image-tagging']});
 
 
 
