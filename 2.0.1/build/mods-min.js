08e6ba2fc85340c60df3ddb76cb629852a8de5ea
弘树 <tiehang.lth@alibaba-inc.com>
2014-09-24 10:53:32 +0800

    sync daily version 2.0.1

---
+++ 2.0.1/build/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-09-24 10:42:24 AM*/
+KISSY.config("modules",{"kg/image-tagging/2.0.0/index":{requires:["dom","event","overlay"]}});
---
+++ 2.0.1/build/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/image-tagging/2.0.0/index': { requires: ['dom', 'event', 'overlay']} 
+});
